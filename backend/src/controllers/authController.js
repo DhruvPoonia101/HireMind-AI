daa1290eb88ade29fc6d594db48e7e3569180462
Dhruv Poonia <dhruvpoonia101@gmail.com>
2026-06-06 00:40:24 +0530

    Added JWT middleware and protected routes

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -85,7 +85,22 @@ const loginUser = async (req, res) => {
     }
 };
 
+const getProfile = async (req, res) => {
+    try {
+        res.status(200).json({
+            success: true,
+            user: req.user,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
 module.exports = {
-    registerUser,
-    loginUser,
+  registerUser,
+  loginUser,
+  getProfile,
 };
