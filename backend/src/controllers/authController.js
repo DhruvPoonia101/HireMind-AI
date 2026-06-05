776241331644b6e2d24abdff44229b69d470addd
Dhruv Poonia <dhruvpoonia101@gmail.com>
2026-06-06 00:12:20 +0530

    Added user model and register API

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
